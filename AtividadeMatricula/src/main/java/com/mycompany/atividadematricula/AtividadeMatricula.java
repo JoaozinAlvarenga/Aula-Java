a79d4ce9f4d85cfe42f36fbe72c72f50d7daf2b7
João Gabriel Alvarenga <159331187+JoaozinAlvarenga@users.noreply.github.com>
2025-03-10 08:48:39 -0300

    para terminar

---
+++ AtividadeMatricula/src/main/java/com/mycompany/atividadematricula/AtividadeMatricula.java
@@ -0,0 +1,24 @@
+package com.mycompany.atividadematricula;
+
+public class AtividadeMatricula {
+
+    public class aluno{
+    String maricula;
+    String nome;
+    String curso;
+    String turma;
+    int periodo;
+    double nota1;
+    double nota2;
+    double notafinal;
+    static int quantidadealunos;
+    }
+    
+    public aluno(String nome, String curso, String turma, int periodo, double nota1, double nota2){
+    
+    }
+
+            
+            }
+        
+
